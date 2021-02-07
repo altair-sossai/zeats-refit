7b84e903bc1670e700268ae09f460cb8866bdf79
Altair Sossai <altairsossai@gmail.com>
2021-02-07 11:23:41 -0300

    Inicio do projeto

---
+++ Zeats.Refit/Context/RefitContext.cs
@@ -0,0 +1,27 @@
+﻿using Refit;
+using Zeats.Json;
+
+namespace Zeats.Refit.Context
+{
+    public class RefitContext
+    {
+        public RefitContext(string apiUrl, RefitSettings refit = null)
+        {
+            ApiUrl = apiUrl;
+            Settings = refit ?? new RefitSettings
+            {
+                ContentSerializer = new SystemTextJsonContentSerializer(JsonConfiguration.Options)
+            };
+        }
+
+        public string ApiUrl { get; set; }
+        public RefitSettings Settings { get; set; }
+
+        public T CreateServer<T>()
+        {
+            return Settings == null
+                ? RestService.For<T>(ApiUrl)
+                : RestService.For<T>(ApiUrl, Settings);
+        }
+    }
+}
