5fb932cb5cf4528ebb56bea3120a71c4fad93919
Altair Sossai <altair_sossai@outlook.com>
2022-01-04 09:48:55 -0800

    upgrade to net6.0

--- Zeats.Refit/Context/RefitContext.cs
+++ Zeats.Refit/Context/RefitContext.cs
@@ -1,27 +1,26 @@
 ﻿using Refit;
 using Zeats.Json;
 
-namespace Zeats.Refit.Context
+namespace Zeats.Refit.Context;
+
+public class RefitContext
 {
-    public class RefitContext
+    public RefitContext(string apiUrl, RefitSettings refitSettings = null)
     {
-        public RefitContext(string apiUrl, RefitSettings refitSettings = null)
+        ApiUrl = apiUrl;
+        Settings = refitSettings ?? new RefitSettings
         {
-            ApiUrl = apiUrl;
-            Settings = refitSettings ?? new RefitSettings
-            {
-                ContentSerializer = new SystemTextJsonContentSerializer(JsonConfiguration.Options)
-            };
-        }
+            ContentSerializer = new SystemTextJsonContentSerializer(JsonConfiguration.Options)
+        };
+    }
 
-        public string ApiUrl { get; set; }
-        public RefitSettings Settings { get; set; }
+    public string ApiUrl { get; set; }
+    public RefitSettings Settings { get; set; }
 
-        public T CreateServer<T>()
-        {
-            return Settings == null
-                ? RestService.For<T>(ApiUrl)
-                : RestService.For<T>(ApiUrl, Settings);
-        }
+    public T CreateServer<T>()
+    {
+        return Settings == null
+            ? RestService.For<T>(ApiUrl)
+            : RestService.For<T>(ApiUrl, Settings);
     }
 }
